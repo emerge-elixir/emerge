4fe1e233857cc36e17bc0341ce4da68584fd3282
Damir Batinovic <damirados@gmail.com>
2026-03-24 16:05:38 +0100

    Different example

--- example/lib/emerge_demo/counter_controller.ex
+++ example/lib/emerge_demo/counter_controller.ex
@@ -3,21 +3,24 @@ defmodule EmergeDemo.CounterController do
 
   use Solve.Controller, events: [:increment, :decrement]
 
+  @min_limit 1
+  @max_limit 99
+
   @impl true
   def init(_params, _dependencies) do
     %{count: 1}
   end
 
   def increment(_payload, state, _dependencies, _callbacks, _init_params) do
-    %{state | count: min(100, state.count + 1)}
+    %{state | count: min(@max_limit, state.count + 1)}
   end
 
   def decrement(_payload, state, _dependencies, _callbacks, _init_params) do
-    %{state | count: max(1, state.count - 1)}
+    %{state | count: max(@min_limit, state.count - 1)}
   end
 
   @impl true
   def expose(%{count: count}, _, _init_params) do
-    %{count: count, can_increment?: count < 100, can_decrement?: count > 1}
+    %{count: count, can_increment?: count < @max_limit, can_decrement?: count > @min_limit}
   end
 end
