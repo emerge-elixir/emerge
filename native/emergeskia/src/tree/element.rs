df2da93cacf8df6dd3df5b4c81e4bfd28068bdeb
Damirados <damirados@gmail.com>
2026-06-03 18:52:50 +0200

    Tree with node insert

--- native/emergeskia/src/tree/element.rs
+++ native/emergeskia/src/tree/element.rs
@@ -3,6 +3,8 @@ use smallvec::SmallVec;
 use std::collections::HashMap;
 
 mod layout;
+mod serde;
+
 use layout::Layout;
 
 new_key_type! {
@@ -75,8 +77,17 @@ struct Attrs {
     font_size: Option<f32>,
 }
 
+#[derive(Debug)]
 enum CreateError {
     RootMustBeContainer,
+}
+
+#[derive(Debug)]
+enum InsertError {
+    DuplicateId,
+    MissingParent,
+    ParentCannotHaveChildren,
+    ElAlreadyHasChild,
 }
 
 impl Tree {
@@ -107,6 +118,51 @@ impl Tree {
             by_element_id: HashMap::from([(id, root)]),
             layout: SecondaryMap::new(),
         })
+    }
+
+    fn insert_node(&mut self, id: Id, kind: Kind, parent: Key) -> Result<Key, InsertError> {
+        if self.by_element_id.contains_key(&id) {
+            return Err(InsertError::DuplicateId);
+        }
+
+        self.validate_parent(parent)?;
+
+        let key = self.elements.insert(Element {
+            id,
+            parent: Some(Parent {
+                key: parent,
+                relation: Relation::Child,
+            }),
+            kind,
+        });
+
+        self.by_element_id.insert(id, key);
+        let parent_el = self.elements.get_mut(parent).expect("parent was validated");
+
+        let Kind::Container { children, .. } = &mut parent_el.kind else {
+            unreachable!("parent was validated as container");
+        };
+
+        children.push(key);
+
+        Ok(key)
+    }
+
+    fn validate_parent(&self, parent: Key) -> Result<(), InsertError> {
+        let parent_el = self
+            .elements
+            .get(parent)
+            .ok_or(InsertError::MissingParent)?;
+
+        match &parent_el.kind {
+            Kind::Container {
+                kind: ContainerKind::El,
+                children,
+                ..
+            } if !children.is_empty() => Err(InsertError::ElAlreadyHasChild),
+            Kind::Container { .. } => Ok(()),
+            Kind::Text { .. } => Err(InsertError::ParentCannotHaveChildren),
+        }
     }
 }
 
--- native/emergeskia/src/tree/element/test.rs
+++ native/emergeskia/src/tree/element/test.rs
@@ -1,5 +1,6 @@
 use super::ContainerKind::El;
-use super::Kind::Container;
+use super::Kind::{Container, Text};
+use super::Tree;
 use super::*;
 
 #[test]
@@ -10,11 +11,21 @@ fn new() {
 
 #[test]
 fn with_root() {
-  let Ok(_) = Tree::with_root(Id(1), empty_el()) else {
-       panic!("expected tree creation to succeed");
-   };
+    let Ok(_) = Tree::with_root(Id(1), empty_el()) else {
+        panic!("expected tree creation to succeed");
+    };
 }
 
+#[test]
+fn insert_node() {
+    let mut tree = tree_with_root();
+    let text = test_text();
+    let Ok(_) = tree.insert_node(Id(2), text, tree.root.expect("should have root")) else {
+        panic!("insert should succeed");
+    };
+}
+
+// Subtree helpers
 fn empty_el() -> Kind {
     Container {
         kind: El,
@@ -30,4 +41,15 @@ fn empty_attrs() -> Attrs {
         spacing: 0.0,
         font_size: None,
     }
+}
+
+fn test_text() -> Kind {
+    Text {
+        content: "Test".into(),
+    }
+}
+
+
+fn tree_with_root() -> Tree {
+    Tree::with_root(Id(1), empty_el()).unwrap()
 }
